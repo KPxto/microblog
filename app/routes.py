6d30eaf71a4e85cf9d89df1a5fab3137bd8ca112
Kpxto <kaioopeixoto@gmail.com>
2021-10-30 19:01:09 -0300

    login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
+from werkzeug.utils import redirect
 from app import app
+from app.form import LoginForm
 
 
 @app.route('/')
@@ -22,3 +24,14 @@ def index():
         user=user, 
         posts=posts
     )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():    
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect('/index')
+
+    return render_template('login.html', title='Sign In', form=form)
+
